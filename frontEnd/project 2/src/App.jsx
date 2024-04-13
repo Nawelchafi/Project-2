add20ff543055c0b0cdfdbb99226447b07260cf6
Bettina <bettike18989@gmail.com>
2024-04-13 16:56:42 +0200

    creat NavBar

--- frontEnd/project 2/src/App.jsx
+++ frontEnd/project 2/src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import NavigationBar from './components/NavigationBar'
+import NavBar from  '../src/components/NavBar'
 import HomePage from './pages/HomePage'
 import ErrorPage from './pages/ErrorPage'
 import AboutPage from './pages/AboutPage'
 import RestaurantPage from './pages/RestaurantPage'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,11 +16,14 @@ function App() {
   return (
     <>
       <div className="App">
-<header>
-          <div>
-            <h1>PROJECT2</h1>
+        <header>
 
-            <NavigationBar />
+          <div>
+            <h1>FLAVERFINDS</h1>
+          
+
+            
+            <NavBar />
           </div>
         </header>
 
